2e1fca7928ca22f87fb3b1ff8cdee3c7721df5b3
reggarcia <mariaregina.garcia@gmail.com>
2014-07-14 10:10:11 -0700

    Update services.js
    updating with heroku server

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('quickTasks.services', [])
     .factory('API', function ($rootScope, $http, $ionicLoading, $window) {
-       var base = "http://localhost:9804";
+       var base = "http://calm-beyond-5882.herokuapp.com/";
         $rootScope.show = function (text) {
             $rootScope.loading = $ionicLoading.show({
                 content: text ? text : 'Loading',
